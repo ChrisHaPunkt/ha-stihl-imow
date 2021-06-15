02df00ca2a935ec751b1e2bf2edac1c2cd1c7bd7
chris <chris@homeset.de>
2021-06-15 19:15:50 +0200

    code formatting

--- custom_components/stihl_imow/__init__.py
+++ custom_components/stihl_imow/__init__.py
@@ -11,21 +11,27 @@ from .const import DOMAIN
 PLATFORMS = ["sensor"]
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(
+        hass: HomeAssistant,
+        entry: ConfigEntry
+) -> bool:
     """Set up STIHL iMow from a config entry."""
     # TODO Store an API object for your platforms to access
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    hass.config_entries.async_setup_platforms(
+        entry, PLATFORMS
+    )
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = \
-        await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        entry, PLATFORMS
+    )
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
